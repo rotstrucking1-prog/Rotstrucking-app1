8d1812599dc19d93a4e20d1b1e692f147ff1e3c5
rotstrucking1-prog <rotstrucking1@gmail.com>
2026-05-27 13:42:00 -0500

    v16b: Fix UE 5.7 forward-declaration includes — GameplayEffect.h + Engine/EngineTypes.h

--- Source/AOC/Characters/AoCCharacterBase.cpp
+++ Source/AOC/Characters/AoCCharacterBase.cpp
@@ -1,7 +1,7 @@
 // Copyright Architect of Creation. All Rights Reserved.
 
 #include "AoCCharacterBase.h"
-#include "GameplayEffectTypes.h"
+#include "GameplayEffect.h"
 #include "Net/UnrealNetwork.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
--- Source/AOC/Spellcraft/AoCSpellCastingComponent.cpp
+++ Source/AOC/Spellcraft/AoCSpellCastingComponent.cpp
@@ -1,7 +1,7 @@
 // Copyright Architect of Creation. All Rights Reserved.
 
 #include "AoCSpellCastingComponent.h"
-#include "WorldCollision.h"
+#include "Engine/EngineTypes.h"
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemInterface.h"
 #include "GameplayEffect.h"

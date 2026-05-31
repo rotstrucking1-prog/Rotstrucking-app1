238cd6ce312f8143f3021b2d0a8f56b523ee7c97
rotstrucking1-prog <rotstrucking1@gmail.com>
2026-05-31 04:30:30 -0500

    TerraForge: FLandscapeEditDataInterface + Foliage module dependency

--- Source/AOC/AOC.Build.cs
+++ Source/AOC/AOC.Build.cs
@@ -55,6 +55,9 @@ public class AOC : ModuleRules
             // Landscape (surface height queries for world generation)
             "Landscape",
 
+            // Foliage (required by LandscapeEdit.h -> InstancedFoliageActor.h)
+            "Foliage",
+
             // Rendering (FlushRenderingCommands for heightmap texture sync)
             "RenderCore",
 
